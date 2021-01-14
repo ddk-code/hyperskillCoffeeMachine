b440c82c7aa983f4f026df2ee17510bf3e96c8e2
pcharlot <pcharlot@student.21-school.ru>
2021-01-15 02:06:43 +0300

    more

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,13 +1,49 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("Starting to make a coffee\n" +
-                "Grinding coffee beans\n" +
-                "Boiling water\n" +
-                "Mixing boiled water with crushed coffee beans\n" +
-                "Pouring coffee into the cup\n" +
-                "Pouring some milk into the cup\n" +
-                "Coffee is ready!");
+//        System.out.println("Write how many cups of coffee you will need: ");
+//        int cups = new Scanner(System.in).nextInt();
+//        System.out.println("For " + cups + " cups of coffee you will need:");
+//        System.out.println(cups * 200 + " ml of water");
+//        System.out.println(cups * 50 + " ml of milk");
+//        System.out.println(cups * 15 + " g of coffee beans");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int water = scanner.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int milk = scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int coffee = scanner.nextInt();
+        System.out.println("Write how many cups of coffee you will need:");
+        int cupsNeeded = scanner.nextInt();
+        int maximumCups = maximumPossibleCups(water, milk, coffee);
+        if (maximumCups >= cupsNeeded) {
+            System.out.print("Yes, I can make that amount of coffee");
+            if (maximumCups > cupsNeeded) {
+                System.out.print(" (and even " + (maximumCups - cupsNeeded) + " more than that)");
+            }
+            System.out.println();
+        } else {
+            System.out.println("No, I can make only " + maximumCups + " cup(s) of coffee");
+        }
+
+    }
+
+    public static int maximumPossibleCups(int water, int milk, int coffee) {
+        int cupsCount = 0;
+        water /= 200;
+        milk /= 50;
+        coffee /= 15;
+        while (water != 0 && milk != 0 && coffee != 0) {
+            --water;
+            --milk;
+            --coffee;
+            ++cupsCount;
+        }
+        return cupsCount;
     }
 }
--- Problems/Check prefix ignoring the case/src/Main.java
+++ Problems/Check prefix ignoring the case/src/Main.java
@@ -3,5 +3,9 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         System.out.println((new Scanner(System.in).nextLine().charAt(0) + "").equalsIgnoreCase("j"));
+        byte a = 1;
+        byte b = 2;
+        byte c = (byte) a + b;
+
     }
 }
